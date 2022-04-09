3d25053a4d174c790831f50a91402d49c1e04045
Nata1304 <92617300+Nata1304@users.noreply.github.com>
2022-04-09 10:08:44 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,37 +6,32 @@ var calc = {
     vvd: null,
 }
 
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var actions = ['plus', 'mine', 'divi', 'mult'];
+
 $(document).ready(function() {
     console.log(calc);
 
+    for (let i = 0; i < array.length; i++) {
+        const e = numbers[i];
 
-    $("#1").click(function() {
-        console.log('#1');
-
-        if (!calc.act) {
-            calc.numbA += '1';
-            showvvd(calc.numbA);
-        } else {
-            calc.numbB += '1';
-            showvvd(calc.numbB);
-        }
-
-        console.log(calc);
-    });   
-
-    $("#2").click(function() {
-        console.log('#2');
-
-        if (!calc.act) {
-            calc.numbA += '2';
-            showvvd(calc.numbA);
-        } else {
-            calc.numbB += '2';
-            showvvd(calc.numbB);
-        }
-
-        console.log(calc);
-    });
+        $("#1").click(function() {
+            console.log('#1');
+    
+            if (!calc.act) {
+                calc.numbA += '1';
+                showvvd(calc.numbA);
+            } else {
+                calc.numbB += '1';
+                showvvd(calc.numbB);
+            }
+    
+            console.log(calc);
+        });   
+    }
+        
+    }
 
     $("#plus").click(function() {
         console.log('#plus');
@@ -52,6 +47,18 @@ $(document).ready(function() {
         calc.isRvn = true;
 
         if (calc.act === 'plus') {
+            calc.vvd = Number(calc.numbA) + Number(calc.numbB);
+        }
+
+        if (calc.act === 'mine') {
+            calc.vvd = Number(calc.numbA) + Number(calc.numbB);
+        }
+
+        if (calc.act === 'divi') {
+            calc.vvd = Number(calc.numbA) + Number(calc.numbB);
+        }
+
+        if (calc.act === 'mult') {
             calc.vvd = Number(calc.numbA) + Number(calc.numbB);
         }
 
